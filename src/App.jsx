9cfdeeb8d4c48872855a580e362e868c519598e3
lutfur rahman siddiquee <121648644+lutfurrahman20@users.noreply.github.com>
2024-10-23 07:38:43 +0600

    navBar toggle added with button

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import DaisyNav from './components/DaisyNav/DaisyNav'
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar></Navbar>
+      <DaisyNav></DaisyNav>
+      <h1 className='text-7xl text-center bg-red-600'>Vite + React</h1>
+
+      
+      
+      
+    </>
+  )
+}
+
+export default App
